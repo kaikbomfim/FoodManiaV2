49a72c98dec7314641522727b7691cdad4d941be
Kaik Bomfim <kaikbomfim@gmail.com>
2024-09-22 23:06:00 -0300

    refactor: generate new build version

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-X44PNF2Y.mjs";import"./chunk-H5PMFP7T.mjs";import"./chunk-YV6EM6UE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BVYKH3NZ.mjs";import"./chunk-H5PMFP7T.mjs";import"./chunk-YV6EM6UE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
